7b3acd66e69d34e4d335c1d67989060c7ccee746
Vanilla HyprArch <vanhyprarch@proton.me>
2026-09-15 15:21:03 +0200

    Use square window corners

--- home/.config/hypr/hyprland.lua
+++ home/.config/hypr/hyprland.lua
@@ -121,7 +121,7 @@ hl.config({
     },
 
     decoration = {
-        rounding       = 10,
+        rounding       = 0,
         rounding_power = 2,
 
         -- Change transparency of focused and unfocused windows
